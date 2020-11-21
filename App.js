1ffa66f67aeb8ef442606b8b2d29b0fa61569d4b
Federico Valles <fedeva_37@hotmail.com>
2020-11-21 16:44:35 -0300

    Styled text and Image added

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={styles.logo} />
+      <Text
+        style={styles.instructions}>To share a photo from your phone with a friend, just press the button below!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +18,16 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
+  logo: {
+    width: 305,
+    height: 159,
+    marginBottom: 10
+  },
+  instructions: {
+    color: '#888',
+    fontSize: 18,
+    marginHorizontal: 15
+  }
 });
